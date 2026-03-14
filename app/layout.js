df542970df550402cfa79feca6f35e45adfa5bc9
Aashra Saleem <160631977+Aashra55@users.noreply.github.com>
2026-03-14 15:43:23 +0500

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,9 @@ import Footer from '../Components/landing/Footer';
 export const metadata = {
   title: 'Corelyn',
   description: 'Developed by Corelyn Developers',
+  icons: {
+    icon: "/icon.png"
+  }
 }
 
 export default function RootLayout({ children }) {
